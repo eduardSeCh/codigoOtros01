c8ded37f691956cf3024f24bc7cdbbea2a857e5b
Eduard <eduardogsec@gmail.com>
2023-02-03 14:20:55 -0600

    [fixed] corregir codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//[fixed] nombre de variables
+//[Fixed] querys
+const namelocal = document.querySelector('.name');
+const blog = document.querySelector('.blog');
+const local = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) {
+  namelocal.textContent = 'cargando...';
+  //[Fixed] Falto colocar try catch
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`); //await de fetch
+    const data = await response.json(); //await de data
+    console.log(data);
+    //[fixed] Las commillas
+    namelocal.textContent = `${data.name}`;
+    blog.textContent = `${data.blog}`;
+    local.textContent = `${data.location}`;
+  } catch (error) {
+    handleError(error)
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  namelocal.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
